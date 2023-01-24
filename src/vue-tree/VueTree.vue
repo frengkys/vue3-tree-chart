f0e2245c39b896a20cbc4ae702de51cf276b9fee
Frengky <frengky@Frengkys-MacBook-Air.local>
2023-01-24 13:13:35 +0700

    chore: update package.json and libs

--- src/vue-tree/VueTree.vue
+++ src/vue-tree/VueTree.vue
@@ -44,7 +44,7 @@ import TreeChartCore, {
   DEFAULT_LEVEL_HEIGHT,
   TreeLinkStyle,
   Direction,
-} from "@ssthouse/tree-chart-core";
+} from "tree-chart-core";
 
 const formatDimension = (dimension) => {
   if (typeof dimension === "number") return `${dimension}px`;
